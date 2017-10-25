7ac38a716e2924af264d2b89b2d812a00577114c
thangs1 <28630889+thangs1@users.noreply.github.com>
2017-10-25 10:30:10 -0400

    Update mavenTemplate.groovy

--- vars/mavenTemplate.groovy
+++ vars/mavenTemplate.groovy
@@ -91,7 +91,7 @@ def call(Map parameters = [:], body) {
 
             podTemplate(cloud: cloud, label: label, inheritFrom: "${inheritFrom}",
                     containers: [
-                            //[name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
+                            [name: 'jnlp', image: "${jnlpImage}", args: '${computer.jnlpmac} ${computer.name}'],
                             [name: 'maven', image: "${mavenImage}", command: '/bin/sh -c', args: 'cat', ttyEnabled: true, privileged: true,
                              envVars: [
                                      [key: 'MAVEN_OPTS', value: '-Duser.home=/root/ -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn'],
